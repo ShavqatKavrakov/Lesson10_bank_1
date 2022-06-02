e32405e9651d10af7089e2999bd6947dd2e039f0
Shavqat <kavrakovshavqatinfo@gmail.com>
2022-06-02 10:42:55 +0300

    добовлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,14 @@
+package types
+
+//Money  представляет собой денежную единицу в минимальных единицах (центы, копейки, дирамы и т.д)
+type Money string
+
+//Category представляет собой категорию, в каторый был совершён платёж (авто, аптеки, рестораны и.т)
+type Category string
+
+//Payment представляет информацию о платёже.
+type Payment struct {
+	ID       int
+	Amount   Money
+	Category Category
+}
