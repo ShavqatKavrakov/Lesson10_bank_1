82390ec44e9b6259b8df0a11cc538866c641a200
Shavqat <kavrakovshavqat@gmail.com>
2022-06-02 16:22:24 +0500

    добовлена функция Avg и TotalInCategory и тест к нему

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -8,23 +8,37 @@ import (
 
 func ExampleAvg() {
 	fmt.Println(Avg([]types.Payment{
-     {
-      Amount: 1_000_00,
-	 },		
-	 {
-		 Amount: 2_000_00,
-	 },
-	 {
-		 Amount: 12_000_00,
-	 },
-	}))
-	fmt.Println(Avg([]types.Payment{	
+		{
+			Amount: 1_000_00,
+		},
+		{
+			Amount: 2_000_00,
+		},
 		{
 			Amount: 12_000_00,
 		},
-	   }))
-	   
-	   //Output:
-	   //500000
-	   //1200000
+	}))
+	fmt.Println(Avg([]types.Payment{
+		{
+			Amount: 12_000_00,
+		},
+	}))
+	fmt.Println(TotalInCategory([]types.Payment{
+		{
+			Amount:   1_000_00,
+			Category: "Restaurant",
+		},
+		{
+			Amount:   2_000_00,
+			Category: "Car",
+		},
+		{
+			Amount:   12_000_00,
+			Category: "Restaurant",
+		},
+	}, "Restaurant"))
+	//Output:
+	//500000
+	//1200000
+	//1300000
 }
